1ce42f520a84767072d4519e98df7242fd1c1b18
Eduardo Aguilar Tirado <eduardo.aguilar@soprasteria.com>
2019-01-18 20:44:19 +0100

    feat: added responsive behavior to restaurant list using a grid.

--- js/main.js
+++ js/main.js
@@ -167,24 +167,28 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const restaurantDataWrapper = document.createElement('div');
+  restaurantDataWrapper.className = 'restaurant-info-wrapper';
+  li.append(restaurantDataWrapper);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  restaurantDataWrapper.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  restaurantDataWrapper.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  restaurantDataWrapper.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  restaurantDataWrapper.append(more);
 
-  return li
+  return li;
 }
 
 /**
